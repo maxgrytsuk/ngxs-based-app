fa0f4b009d721c5ece7cf74f7962756d2c380d80
Max Grytsuk <max.grytsuk@gmail.com>
2019-09-24 11:18:31 +0300

    add ngxs store

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,8 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { NgxsModule } from '@ngxs/store';
+
 @NgModule({
   declarations: [
     AppComponent
@@ -12,7 +14,9 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    NgxsModule.forRoot([
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
