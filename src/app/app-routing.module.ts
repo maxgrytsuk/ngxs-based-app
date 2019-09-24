cbf81d8d4a603abf9c646da7d00a1928ee19a95a
Max Grytsuk <max.grytsuk@gmail.com>
2019-09-24 11:30:37 +0300

    clean up, add provider component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ProviderComponent } from './provider/provider.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path: '', component: ProviderComponent
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,12 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { NgxsModule } from '@ngxs/store';
+import { ProviderComponent } from './provider/provider.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProviderComponent
   ],
   imports: [
     BrowserModule,
